d7f14fbcec71e64d4280c37fef0e136c05533889
finnbear <finnbearone@gmail.com>
2017-12-28 20:57:35 -0800

    Clarified turntable code.

--- turntable/turntable.ino
+++ turntable/turntable.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-int shots = 36;
-
+// How many pictures to take
 int howMany = 20;
-int howLong = 250;
 
+// How long to rotate the turntable per picture
+int howLong = 250;
 
 Servo servo;
 
@@ -17,16 +17,19 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < howMany; i++) {
+    // Rotate the turntable into position
     servo.write(20);
     delay(howLong);
     servo.write(0);
     delay(250);
 
+    // Take a picture
     pinMode(8, OUTPUT);
     digitalWrite(8, LOW);
     delay(25);
     pinMode(8, INPUT);
-    
+
+    // Wait for the picture
     delay(2000);
   }
 
